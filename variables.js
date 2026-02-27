f5a2edaebd6d159cf45f100f9743e1b6443aa120
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-02-28 01:47:37 +0500

    changes

---
+++ variables.js
@@ -0,0 +1,8 @@
+const accountId = 123;
+let accountEmail = "aun@gmail.com";
+var accountPassword = "abc";
+accountCity = "Karachi";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
