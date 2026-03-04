4e756b5020f76e30038286bbd3daea68fb0f18f5
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-03-05 00:23:00 +0500

    pushed

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 let myArray = ["orange","mango","banana"];
 console.log(myArray.length);
-for(let i = 0; i < myArray.length; i++){
+for(let i = 0; i <= myArray.length; i++){
     console.log(myArray[i]);
 };
