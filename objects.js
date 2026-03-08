54a3c071376baa26652991d416086c60165963a1
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-03-09 00:14:25 +0500

    objects

---
+++ objects.js
@@ -0,0 +1,19 @@
+const myObj = {
+    name: "Aun",
+    age: 20,
+    location: "karachi",
+    email: "aunzaidi@gmail.com",
+    IsMale: true
+};
+
+console.log(myObj.name);
+console.log(myObj.email);
+console.log(myObj.location);
+console.log(myObj.email);
+console.log(myObj.IsMale);
+
+
+
+
+
+
