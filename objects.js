8a9ea8fb7c9cb3f81ddd7ba295e45588fd19ae50
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-03-09 20:34:11 +0500

    objects

--- objects.js
+++ objects.js
@@ -1,9 +1,11 @@
+const mySym = Symbol("MyKey1");
 const myObj = {
     name: "Aun",
     age: 20,
     location: "karachi",
     email: "aunzaidi@gmail.com",
-    IsMale: true
+    IsMale: true,
+    mySym: "Key1"
 };
 
 console.log(myObj.name);
@@ -11,9 +13,10 @@ console.log(myObj.email);
 console.log(myObj.location);
 console.log(myObj.email);
 console.log(myObj.IsMale);
+console.log(myObj.mySym);
 
 
-
+console.log(mySym);
 
 
 
