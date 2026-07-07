8b4bb27be1b76b0e0a21831c94cdf8dc3f656e68
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-07-07 22:02:09 +0500

    arrow

--- practice.js
+++ practice.js
@@ -1,120 +1,159 @@
-// let myArray = [1,2,3,4,5];
-// console.log(myArray);
-// console.log(myArray[0]);
-// console.log(typeof(myArray));
+// // let myArray = [1,2,3,4,5];
+// // console.log(myArray);
+// // console.log(myArray[0]);
+// // console.log(typeof(myArray));
 
 
-// let myObj = {
+// // let myObj = {
+// //     name: "Aun",
+// //     age: 22,
+// //     rollNo: "SE-23095",
+// // };
+// // console.log(myObj.name);
+// // console.log(myObj);
+
+
+// // let myFunction = function(){
+// //     console.log("Aun Zaidi");
+    
+// // }
+// // myFunction();
+
+// // console.log(myArray.unshift(4));
+// // console.log(myArray.shift);
+
+// // console.log(myArray);
+
+// var id = Symbol("id")
+// var myObj = {
+//     [id]: 101,
 //     name: "Aun",
 //     age: 22,
-//     rollNo: "SE-23095",
+//     LoggedIn: true,
+//     email: "aunzaidi@gmail.com",
 // };
-// console.log(myObj.name);
-// console.log(myObj);
+
+// var myObjs = {
+//     [id]: 101,
+//     name: "Aunzee",
+//     age: 22,
+//     LoggedIn: true,
+//     email: "aunzaidi@gmail.com",
+// };
 
 
-// let myFunction = function(){
-//     console.log("Aun Zaidi");
+// // console.log(myObj.age);
+// // // console.log(myObj[age]);
+// // console.log(myObj["age"]);
+// // console.log(myObj[id]);
+// // // console.log(typeof mysym);
+// // console.log(myObjs[id]);
+
+
+// myObj.greeting = function(){
+//     console.log(`hello ${myObjs.name}`);
     
 // }
-// myFunction();
 
-// console.log(myArray.unshift(4));
-// console.log(myArray.shift);
+// // myObj.greeting();
 
-// console.log(myArray);
+// var course = {
+//     coursename: "JavaScript",
+//     courseprice: "1200rs",
+//     courseteacher: "Hitesh"
+// }
 
-var id = Symbol("id")
-var myObj = {
-    [id]: 101,
-    name: "Aun",
-    age: 22,
-    LoggedIn: true,
-    email: "aunzaidi@gmail.com",
-};
+// var {coursename:names} = course;
+// var {courseprice:price} = course;
+// var {courseteacher:teacher} = course;
 
-var myObjs = {
-    [id]: 101,
-    name: "Aunzee",
-    age: 22,
-    LoggedIn: true,
-    email: "aunzaidi@gmail.com",
-};
+// // console.log(teacher);
+// // console.log(names);
+// // console.log(price);
 
+// // ;
+// // function add(a,b){
+// //     console.log(a+b);
+// // };
 
-// console.log(myObj.age);
-// // console.log(myObj[age]);
-// console.log(myObj["age"]);
-// console.log(myObj[id]);
-// // console.log(typeof mysym);
-// console.log(myObjs[id]);
+// // add(2,3);
 
-
-myObj.greeting = function(){
-    console.log(`hello ${myObjs.name}`);
+// function loginMessage (username = "user"){
+//     return `${username} just logged in!`;
     
-}
-
-// myObj.greeting();
-
-var course = {
-    coursename: "JavaScript",
-    courseprice: "1200rs",
-    courseteacher: "Hitesh"
-}
-
-var {coursename:names} = course;
-var {courseprice:price} = course;
-var {courseteacher:teacher} = course;
-
-// console.log(teacher);
-// console.log(names);
-// console.log(price);
-
-// ;
-// function add(a,b){
-//     console.log(a+b);
 // };
 
-// add(2,3);
+// console.log(loginMessage("aa"));
 
-function loginMessage (username = "user"){
-    return `${username} just logged in!`;
+// function cartPrice(...price){
+//     return price;
+// }
+// console.log(cartPrice(100,200,399,356,346,22,6,4,7,44,57,4,6,3,6,6,3,5,7,3,6,3,6,234));
+
+// var arr1 = [1,2,3,4];
+// var arr2 = [5,6,7,8];
+// var arr = [...arr1,...arr2];
+// // console.log(arr);
+
+// var user = {
+//     name: "Aun",
+//     price: 200
+// };
+
+// function obj(anyobj){
+//     console.log(`the name is ${anyobj.name} and the price is ${anyobj.price}`);
+// }
+
+// obj(user);
+
+// obj({
+//     name:"Ali",
+//     price: 233
+// })
+
+// var newArr = [1,2,3,4,5];
+
+// function getSecondValue(getArr){
+//     return getArr[1];
+// }
+
+// console.log(getSecondValue(newArr));
+
+
+// const myobj = {
+//     name: "Aun",
+//     price: 440,
     
-};
+//     welcomeMessage: function(){
+//         console.log(`${this.name} and ${this.price}`);  
+//     }
+// }
 
-console.log(loginMessage("aa"));
+// myobj.welcomeMessage();
+// console.log(this);
 
-function cartPrice(...price){
-    return price;
+
+// const arrow1 = function(){
+//     let a = "aun";
+//     console.log(this.a);
+    
+// }
+
+// const arrow2 = () => {
+//     let a = "zaidi";
+//     console.log(this.a);
+// }
+
+// arrow1();
+// arrow2();
+
+
+
+(function add(num1, num2){
+    console.log(num1+num2);
 }
-console.log(cartPrice(100,200,399,356,346,22,6,4,7,44,57,4,6,3,6,6,3,5,7,3,6,3,6,234));
+)(1,2);
 
-var arr1 = [1,2,3,4];
-var arr2 = [5,6,7,8];
-var arr = [...arr1,...arr2];
-// console.log(arr);
-
-var user = {
-    name: "Aun",
-    price: 200
-};
-
-function obj(anyobj){
-    console.log(`the name is ${anyobj.name} and the price is ${anyobj.price}`);
-}
-
-obj(user);
-
-obj({
-    name:"Ali",
-    price: 233
-})
-
-var newArr = [1,2,3,4,5];
-
-function getSecondValue(getArr){
-    return getArr[1];
-}
-
-console.log(getSecondValue(newArr));
+((num1,num2) => {
+    console.log(num1+num2);   
+})(1,2);
