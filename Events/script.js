8f86f63548f9dc24c76ae9b7edbe8912c3b77b5e
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-07-16 21:27:23 +0500

    EventsBasics

--- Events/script.js
+++ Events/script.js
@@ -4,11 +4,18 @@ document.querySelector('.images').addEventListener('click', function(e){
 
 document.querySelector('.i2').addEventListener('click', function(e){
     console.log("image 2 clicked");  
-    e.stopPropagation()
+    // e.stopPropagation()
 }, false)
 
 document.querySelector('.google').addEventListener('click', function(e){
     console.log("link clicked");  
     e.preventDefault()
     e.stopPropagation()
+}, false)
+
+
+document.querySelector('.images').addEventListener('click', function(e){
+    console.log(e.target.parentNode); 
+    let removeIt = e.target.parentNode;
+    removeIt.remove() 
 }, false)
