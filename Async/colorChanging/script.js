346464b2f8b229c07c65f5863c90088ac453ceb0
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-07-16 22:50:36 +0500

    AsyncBasics

---
+++ Async/colorChanging/script.js
@@ -0,0 +1,25 @@
+const randomColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+console.log(randomColor())
+
+const start = document.querySelector('.start')
+const stop = document.querySelector('.stop')
+
+const colorChange = function(){
+    document.body.style.backgroundColor = randomColor();
+}
+
+let int;
+start.addEventListener('click', function(e){
+    int = setInterval(colorChange,500)
+})
+
+stop.addEventListener('click', function(e){
+    clearInterval(int)
+})
