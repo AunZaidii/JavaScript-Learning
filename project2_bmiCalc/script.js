7ab8349802fec78a3c2b6e0e859b0dd3d60d118f
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-07-16 15:46:01 +0500

    bmiCalc

--- project2_bmiCalc/script.js
+++ project2_bmiCalc/script.js
@@ -15,6 +15,17 @@ form.addEventListener('submit', function(e){
     }
     else{
         const bmi = ((weight/(height/100)**2)).toFixed(2)
-        result.innerHTML = `Your BMI is ${bmi}`
+        
+        if(bmi < 18.6){
+            result.innerHTML = `your bmi is ${bmi}, you are underweight`
+        }
+        else if(bmi > 18.6 && bmi <= 24.9){
+            result.innerHTML = `your bmi is ${bmi}, you are normal`
+        }
+        else{
+            result.innerHTML = `your bmi is ${bmi}, you are overweight`
+        }
     }
+
+
 }) 
