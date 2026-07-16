a5fcab456c75047491580cbc7b7c5e31ef09420f
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-07-16 15:51:23 +0500

    bmiCalc

--- project2_bmiCalc/script.js
+++ project2_bmiCalc/script.js
@@ -2,8 +2,8 @@ const form = document.querySelector("form")
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    const height = parseInt(document.querySelector("#height").value)
-    const weight = parseInt(document.querySelector("#weight").value)
+    const height = (document.querySelector("#height").value)
+    const weight = (document.querySelector("#weight").value)
     const result = document.querySelector("#result")
 
 
