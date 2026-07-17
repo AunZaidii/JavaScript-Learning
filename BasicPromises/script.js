1bf8988d4c68ba561df014819f718742efe709d3
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-07-17 12:34:56 +0500

    promises

--- BasicPromises/script.js
+++ BasicPromises/script.js
@@ -66,15 +66,25 @@
 // }
 // promiseFive();
 
-async function getAllUsers(){
-    try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/users")
-        const data = await response.json()
-        console.log(data);
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//         const data = await response.json()
+//         console.log(data);
     
-    } catch (error) {
-        console.log("there is an error: ", error);
+//     } catch (error) {
+//         console.log("there is an error: ", error);
         
-    }
-}
-getAllUsers()
+//     }
+// }
+// getAllUsers()
+
+fetch("https://jsonplaceholder.typicode.com/users").then(function(response){
+    return response.json()
+}).then(function(data){
+    console.log(data);
+})
+.catch(function(error){
+    console.log("the error is: ",error);
+    
+})
