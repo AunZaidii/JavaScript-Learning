c06f8ebd0372896112d9132ad50f1fba4d4b5f05
AunZeee <148131524+AunZaidii@users.noreply.github.com>
2026-07-17 13:43:54 +0500

    promises

--- OOP/script.js
+++ OOP/script.js
@@ -9,13 +9,35 @@
     
 // }
 
-function user (username,LoginCount, isLoggedIn ){
-    this.username = username;
-    this.LoginCount = LoginCount;
-    this.isLoggedIn = isLoggedIn;
+// function user (username,LoginCount, isLoggedIn ){
+//     this.username = username;
+//     this.LoginCount = LoginCount;
+//     this.isLoggedIn = isLoggedIn;
+// }
+
+// const userOne = new user('aun',33,true)
+// console.log(userOne);
+// const userTwo = new user('aun',33,true)
+// console.log(userTwo);
+
+
+const users = {
+   totalUsers : []
 }
 
-const userOne = new user('aun',33,true)
-console.log(userOne);
-const userTwo = new user('aun',33,true)
-console.log(userTwo);
+function user(userId, username, userAge){
+    this.userId = userId;
+    this.username = username;
+    this.userAge = userAge;
+}
+
+function addUsers(userId, username, userAge){
+    const newUser = new user(userId, username, userAge)
+    users.totalUsers.push(newUser)
+
+}
+addUsers(1,'batool', true);
+addUsers(2,'Aun', true);
+addUsers(3,'Haris', true);
+
+console.log(users.totalUsers);
